97d1b2b2c6b1833a2b6e337bc5126f945c0fa518
Amber <amberolson@trentu.ca>
2016-09-21 14:31:18 -0400

    fixing ruby idioms in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,23 @@
 class AdminController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_event
+  before_action :authenticate_user!
+  before_action :set_event
 
-    def new
-        @membership = Membership.where(:event_id => @event.id)
+  def new
+    @membership = Membership.where(:event_id => @event.id)
+  end
+
+  def create
+    activated_ids = params[:activated].collect { |id| id.to_i }
+    activated_ids.each do |id|
+      Membership.update(id, :admin => true)
     end
+    redirect_to @event
+  end
 
-    def create
-        activated_ids = params[:activated].collect {|id| id.to_i} if params[:activated]
-        activated_ids.each do |id|
-            Membership.update(id, {:admin => true})
-        end
-        redirect_to @event
-    end
+  private
 
-    private
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 
-    def set_event
-        @event = Event.find(params[:event_id])
-    end
 end
