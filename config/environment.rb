2d4b0ed5324419f8af3ad93e3acbf1baf77ab5c6
Amber <amberolson@trentu.ca>
2016-10-09 19:27:02 -0400

    added simplecov"

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+require 'simplecov'
+SimpleCov.start
+
 # Initialize the Rails application.
 PartyTime::Application.initialize!
