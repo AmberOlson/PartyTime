918cbfbd5b91c64c8c87d70b68b9182a4da783d0
Amber <amberolson@trentu.ca>
2016-10-24 18:04:00 -0400

    changes to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,21 +9,13 @@ class UserMailer < ActionMailer::Base
     @user = user
     @event = event
     @membership = membership
-    mail(to: @user, subject: 'Welcome to My Awesome Site')
+    mail to: @user, subject: 'Welcome to My Awesome Site'
   end
 
   def membership_email(user, event)
     @user = user
     @event = event
-    #mail(to: @user, subject: 'You have been invited to an event')
     mail to: @user, subject: 'You have been invited to an event'
-
-    #RestClient.post "https://api:api:key-52542e3e32bb2ce0bc90532a295b68f4"\
-    #{}"@api.mailgun.net/v3/sandboxa57f7da321ce41bf9aba1a361c67ca86/messages",
-    #:from => "PartyTime <postmaster@sandboxa57f7da321ce41bf9aba1a361c67ca86.mailgun.org>",
-    #:to => @user,
-    #:subject => 'You have been invited to an event',
-    #:text => "Testing some Mailgun awesomness!"
   end
 
 
@@ -32,7 +24,7 @@ class UserMailer < ActionMailer::Base
     @event = event
     parsed_json = weather(@event.lat, @event.lng)
     @temp_f = parsed_json['forecas']['txt_forecast']['forecastday'][2]['fcttext']
-    mail(to: @user, subject: 'Your event is tomorrow')
+    mail to: @user, subject: 'Your event is tomorrow'
   end
 
   def weather(lat, lng)
